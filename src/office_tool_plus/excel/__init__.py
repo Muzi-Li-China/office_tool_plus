1aed9181c1ac72f2b86d1638362c1662e8a4c156
李小涛 <1537080775@qq.com>
2025-03-26 11:04:16 +0800

    feat(excel): 添加模板生成Excel功能
    - 在 excel 模块中添加了 from_template 函数，用于根据模板生成 Excel 文件
    - 更新了 pyproject.toml 文件，将项目版本号从 0.1.1 升级到0.1.2
    -同时将 docxtpl 依赖的版本从 0.19.1 降级到 0.16.8，可能是为了兼容性或其他需求

--- src/office_tool_plus/excel/__init__.py
+++ src/office_tool_plus/excel/__init__.py
@@ -9,3 +9,7 @@ def single_to_pdf(excel_path, sheet_names=None, pdf_dir=None):
 
 def many_to_pdf(excel_dir, suffix=None, recursive=True, pdf_dir=None):
     return excel.many_to_pdf(excel_dir, suffix, recursive, pdf_dir)
+
+
+def from_template(template_file, labor_datas, sheet_name=None, output_dir=None):
+    return excel.from_template(template_file, labor_datas, sheet_name, output_dir)
